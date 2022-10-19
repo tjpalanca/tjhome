5a1cd80255f82995b3350d23f02fa02fc57b7f34
TJ P <mail@tjpalanca.com>
2022-10-19 02:17:48 +0000

    Keycloak URL

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,5 +2,5 @@ provider "keycloak" {
   client_id = "admin-cli"
   username  = var.keycloak_admin_username
   password  = var.keycloak_admin_password
-  url       = var.keycloak_url
+  url       = local.keycloak_url
 }
